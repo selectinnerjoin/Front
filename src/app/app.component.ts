42e52775a2f215c4ae8e9ba3ef41ac2c0f404c0a
selectinnerjoin <pmoya@ng-cloud.cl>
2025-09-01 08:21:38 -0400

    Initial commit v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
-import { Component, Inject, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, Inject, ViewChild, AfterViewInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser, CommonModule } from '@angular/common';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
-import { CommonModule } from '@angular/common';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { Observable, switchMap } from 'rxjs';
 import { MatSelectModule } from '@angular/material/select';
 
 interface User {
@@ -42,33 +42,83 @@ interface User {
 })
 export class AppComponent implements AfterViewInit {
   title = 'UserFront';
-  displayedColumns: string[] = ['id', 'name', 'isComplete', 'actions'];
+  displayedColumns: string[] = ['id', 'name', 'email', 'password', 'username', 'isComplete', 'actions'];
   dataSource = new MatTableDataSource<User>([]);
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-
-  constructor(private dialog: MatDialog, private http: HttpClient) { }
+  private apiUrl = 'http://localhost:8080/api/todo';
+  accessToken: any;
+  constructor(private dialog: MatDialog, private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) { }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-    this.loadUsers();
+    if (isPlatformBrowser(this.platformId)) {
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
+      // Iniciar el proceso de autenticación y carga de usuarios.
+      this.loginAndLoadUsers();
+    }
+  }
+
+  private getAuthHeaders(): HttpHeaders {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = this.accessToken;// localStorage.getItem('authToken');
+      return new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      });
+    }
+    return new HttpHeaders({ 'Content-Type': 'application/json' });
+  }
+
+  loginAndLoadUsers() {
+    // Definir las credenciales para Basic Auth
+    const usernameAngularApp = 'angularapp';
+    const passwordAngularApp = '12345';
+    const basicAuthString = btoa(`${usernameAngularApp}:${passwordAngularApp}`);
+
+    // Crear las cabeceras con la autenticación Basic Auth, tal como en el código C#
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Authorization': `Basic ${basicAuthString}`
+    });
+
+    // Definir los datos de login para el usuario "admin"
+    const loginData = { username: "admin", password: "admin" };
+
+    // Realizar la petición POST con ambas credenciales en un solo paso
+    this.http.post<any>('http://localhost:8080/api/auth/signin', loginData, { headers }).subscribe(
+      response => {
+        if (isPlatformBrowser(this.platformId)) {
+          // Guardar el token de acceso final
+          this.accessToken = response.token.replace('Bearer ', '');
+          localStorage.setItem('authToken', this.accessToken);
+          console.log('Login exitoso. Token guardado.');
+        }
+        // Cargar los usuarios con el token recién obtenido
+        this.loadUsers();
+      },
+      error => {
+        console.error('Error en el proceso de autenticación o carga de usuarios:', error);
+      }
+    );
   }
 
   loadUsers() {
-    this.http.get<User[]>('http://localhost:5000/api/Todo').subscribe(data => {
+    this.http.get<User[]>(this.apiUrl, { headers: this.getAuthHeaders() }).subscribe(data => {
       this.dataSource.data = data;
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
+      if (isPlatformBrowser(this.platformId)) {
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
     });
   }
 
   getUserById(id: number): Observable<User> {
-    return this.http.get<User>(`http://localhost:5000/api/Todo/${id}`);
+    return this.http.get<User>(`${this.apiUrl}/${id}`, { headers: this.getAuthHeaders() });
   }
 
   createUser(user: User) {
-    this.http.post<User>('http://localhost:5000/api/Todo', user).subscribe(() => {
+    this.http.post<User>(this.apiUrl, user, { headers: this.getAuthHeaders() }).subscribe(() => {
       this.loadUsers();
     });
   }
@@ -81,7 +131,7 @@ export class AppComponent implements AfterViewInit {
 
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.http.put(`http://localhost:5000/api/Todo/${user.id}`, result).subscribe(() => {
+        this.http.put(`${this.apiUrl}/${user.id}`, result, { headers: this.getAuthHeaders() }).subscribe(() => {
           this.loadUsers();
         });
       }
@@ -90,7 +140,7 @@ export class AppComponent implements AfterViewInit {
 
   delete(user: User) {
     if (confirm(`¿Eliminar usuario ${user.name}?`)) {
-      this.http.delete(`http://localhost:5000/api/Todo/${user.id}`).subscribe(() => {
+      this.http.delete(`${this.apiUrl}/${user.id}`, { headers: this.getAuthHeaders() }).subscribe(() => {
         this.loadUsers();
       });
     }
@@ -127,8 +177,20 @@ export class AppComponent implements AfterViewInit {
     <h2 mat-dialog-title>{{ data.id ? 'Editar' : 'Crear' }} Usuario</h2>
     <mat-dialog-content>
       <mat-form-field class="width-100">
-        <mat-label>Nombre</mat-label>
+        <mat-label>Nombre Completo</mat-label>
         <input matInput [(ngModel)]="data.name">
+      </mat-form-field>
+      <mat-form-field class="width-100">
+        <mat-label>Nombre de Usuario</mat-label>
+        <input matInput [(ngModel)]="data.username">
+      </mat-form-field>
+      <mat-form-field class="width-100">
+        <mat-label>Correo</mat-label>
+        <input matInput [(ngModel)]="data.email" type="email">
+      </mat-form-field>
+      <mat-form-field class="width-100">
+        <mat-label>Contraseña</mat-label>
+        <input matInput [(ngModel)]="data.password" type="password">
       </mat-form-field>
       <mat-form-field class="width-100">
         <mat-label>Completado</mat-label>
@@ -158,5 +220,3 @@ export class UserEditDialogComponent {
     this.dialogRef.close(this.data);
   }
 }
-
-
